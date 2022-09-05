b9e74d0ed0de66f0eb26a9562423903ffd295c0a
l00sed <danielwtompkins@gmail.com>
2022-09-05 16:19:29 -0400

    Changing image naming conventions and directory hierarchy. Removed some files from git version control. Added some files into gitignore.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -126,5 +126,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
-MEDIA_URL = '/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '')
+MEDIA_URL = '/'
